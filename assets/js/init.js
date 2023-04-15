06b52291360f6109799a1a21c3e3437d4fc9c243
Furkan Uzumcu <furkanuzumcu@gmail.com>
2023-04-15 16:09:43 -0400

    Use giscus

--- assets/js/init.js
+++ assets/js/init.js
@@ -4,19 +4,6 @@ let ALIGNMENT_INDEX = 1
 const THEME_OPTIONS = ["light", "dark", "auto"]
 let THEME_INDEX = 2
 
-const gitalk = new Gitalk({
-    clientID: '9636625723440c90efd5',
-    clientSecret: '9d484415b6c004ac58333cf773adf32885688c25',
-    repo: 'zmc.space.comments',
-    owner: 'Furkanzmc',
-    admin: ['Furkanzmc'],
-    id: location.pathname,
-    distractionFreeMode: true
-})
-
-gitalk.render('gitalk-container')
-
-
 function setAlignment(alignment) {
     const button = document.querySelector('#alignment-button');
     button.innerHTML = alignment;
